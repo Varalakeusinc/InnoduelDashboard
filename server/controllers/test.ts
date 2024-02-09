7a262cd8442c714b4ae5eab6af3d802f0bd40655
temelehtone <91484193+temelehtone@users.noreply.github.com>
2024-02-09 16:01:26 +0200

    Add mock data folder to server

--- server/controllers/test.ts
+++ server/controllers/test.ts
@@ -1,22 +1,7 @@
 import { Request, Response } from 'express';
-import { Test } from '../models/test';
+import { getTestMockData } from '../mockData/test';
 
-let tests: Test[] = [
-    {
-        id: 1,
-        title: "Test",
-        description: "This is a test",
-        completed: false
-    },
-    {
-        id: 2,
-        title: "Test 2",
-        description: "This is a test 2",
-        completed: false
-    
-    }
-];
 
 export const getAll = async (req: Request, res: Response) => {
-    res.status(200).json(tests);
+    res.status(200).json(getTestMockData);
 }
