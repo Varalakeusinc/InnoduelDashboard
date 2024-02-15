a583a7d6ce3d3f44e4a3484663a9867587052014
temelehtone <91484193+temelehtone@users.noreply.github.com>
2024-02-15 20:57:59 +0200

    Init prisma database

---
+++ db.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run Docker Compose
+docker-compose --env-file server/.env up -d
--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,3 +1,11 @@
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
-export const db = new PrismaClient();
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
