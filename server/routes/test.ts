9a7161560e1d55122591d2fd9b6b1d11f85859a9
Teemu Lehtonen <teemu.lehtonen@vertex.fi>
2024-02-21 10:21:37 +0200

    Remove test route from backend

--- server/routes/test.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getAll, getById } from '../controllers/test';
-
-const router = Router();
-
-router.get('/', getAll);
-router.get('/:id', getById);
-
-export default router;
