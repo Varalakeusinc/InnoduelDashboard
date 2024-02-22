a6f451439f88c37c78937425481c5471cdd28ec3
temelehtone <91484193+temelehtone@users.noreply.github.com>
2024-02-22 17:27:33 +0200

    Add unknown endpoint middleware

--- server/app.ts
+++ server/app.ts
@@ -28,6 +28,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // Routes
 app.use('/arena', arenaRoutes);
 
+app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
 export default app;
