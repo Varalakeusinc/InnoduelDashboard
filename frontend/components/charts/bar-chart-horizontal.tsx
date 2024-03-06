3a922229fb808755b990ac169ac5e3fb31a32085
Lotta Lappalainen <lotta.lappalainen@tuni.fi>
2024-03-06 20:37:23 +0200

    testArena page

---
+++ frontend/components/charts/bar-chart-horizontal.tsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useRef } from "react";
+import Chart from "chart.js/auto";
+import { MockArena } from "@/src/services/arena";
+
+interface Props {
+  arena: MockArena;
+}
+
+const ChartBarHorizontal: React.FC<Props> = ({ arena }) => {
+  const chartRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (chartRef && chartRef.current) {
+      const sortedIdeas = [...arena.ideas].sort((a, b) => b.votes - a.votes); // Sort ideas by votes in descending order
+      const data = sortedIdeas.map((idea) => idea.votes); 
+      const labels = sortedIdeas.map((idea) => ""); 
+
+      const chartConfig = {
+        type: "bar",
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              label: "Votes",
+              data: data,
+              backgroundColor: "#4371CD", 
+              hoverBackgroundColor: "#185592", 
+            },
+          ],
+        },
+        options: {
+          indexAxis: "y",
+          plugins: {
+            title: { 
+                display: true,
+                text: "Votes by Idea", 
+                font: {
+                  size: 18
+                },
+                padding: {
+                  top: 10,
+                  bottom: 10
+                }
+              },
+            tooltip: {
+              callbacks: {
+                label: function (context: any) {
+                  const label = sortedIdeas[context.dataIndex].name; 
+                  const value = context.dataset.data[context.dataIndex];
+                  if (value !== null) {
+                    return label + ": " + value;
+                  }
+                  return "";
+                },
+              },
+            },
+          },
+          scales: {
+            x: {
+              stacked: true,
+              display: true,
+              grid: {
+                display: true,
+                borderDash: [2],
+                zeroLineColor: "rgba(0,0,0,0)",
+                zeroLineBorderDash: [2],
+                zeroLineBorderDashOffset: [2],
+              },
+              ticks: {
+                color: "rgba(0,0,0, 0.5)",
+              },
+            },
+            y: {
+              stacked: true,
+              grid: {
+                display: false,
+              },
+              ticks: {
+                color: "rgba(0,0,0, 0.5)",
+              },
+            },
+          },
+        },
+      };
+
+      const chartInstance = new Chart(chartRef.current, chartConfig as any);
+
+      return () => {
+        chartInstance.destroy();
+      };
+    }
+  }, [arena]); // Re-render chart when arena changes
+
+  return <canvas ref={chartRef} />;
+};
+
+export default ChartBarHorizontal;
--- frontend/src/pages/testArena.tsx
+++ frontend/src/pages/testArena.tsx
@@ -1,7 +1,34 @@
+import React from "react";
+import ChartBarHorizontal from "@/components/charts/bar-chart-horizontal";
+import { mockArenas } from "@/src/services/arena";
 
 const TestArena = () => {
-	return <h1>Testing:</h1>;
+    const selectedArena = mockArenas[2]; 
 
+    return (
+        <div className="mt-8"> 
+			<h1 className="text-3xl font-semibold mb-4">{selectedArena.name}</h1> 
+			<div className="grid grid-cols-4 gap-4 mb-8"> 
+                <div className="p-4 bg-violet-800 rounded-xl shadow-md flex flex-col items-center"> 
+                    <span className="text-3xl font-bold text-white">{selectedArena.totalVotes}</span> 
+                    <span className="text-white">Vote amount</span>
+                </div>
+                <div className="p-4 bg-fuchsia-800 rounded-xl shadow-md flex flex-col items-center"> 
+                    <span className="text-3xl font-bold text-white">{selectedArena.totalIdeas}</span> 
+                    <span className="text-white">Idea amount</span>
+                </div>
+                <div className="p-4 bg-purple-800 rounded-xl shadow-md flex flex-col items-center"> 
+                    <span className="text-3xl font-bold text-white">{selectedArena.totalVoters}</span> 
+                    <span className="text-white">Voter amount</span>
+                </div>
+                <div className="p-4 bg-pink-800 rounded-xl shadow-md flex flex-col items-center"> 
+                    <span className="text-3xl font-bold text-white">{selectedArena.winRate} %</span> 
+                    <span className="text-white">Win rate</span>
+                </div>
+			</div>
+            <ChartBarHorizontal arena={selectedArena} /> {/* Pass selected arena as prop */}
+        </div>
+    );
 };
 
 export default TestArena;
