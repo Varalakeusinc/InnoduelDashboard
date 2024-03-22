9c1844de30776b37e5cdd7b2928c3374fd954659
Danielevaccari <daniele.j.vaccari@gmail.com>
2024-03-22 10:16:50 +0200

    enhancement

--- frontend/components/charts/bar-chart-horizontal.tsx
+++ frontend/components/charts/bar-chart-horizontal.tsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useRef } from "react";
+import * as React from "react";
 import Chart from "chart.js/auto";
 import { Idea } from "@/src/services/ideas";
 
-interface Props {
+type ChartBarHorizontalProps = {
 	ideas: Idea[];
-}
+};
 
-const ChartBarHorizontal: React.FC<Props> = ({ ideas }) => {
-	const chartRef = useRef<HTMLCanvasElement>(null);
+const ChartBarHorizontal: React.FC<ChartBarHorizontalProps> = ({ ideas }) => {
+	const chartRef = React.useRef<HTMLCanvasElement>(null);
 
-	useEffect(() => {
+	React.useEffect(() => {
 		if (chartRef && chartRef.current) {
 			const sortedIdeas = [...ideas].sort(
 				(a, b) => b.vote_count - a.vote_count
