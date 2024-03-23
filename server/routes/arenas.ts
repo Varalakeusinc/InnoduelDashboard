ce6a75b0e5d86753ae81ea483e873d78671f2591
Aynur Rahman Talukdar <128217234+dareDEVil-aynur@users.noreply.github.com>
2024-03-23 15:05:24 +0200

    add routes

--- server/routes/arenas.ts
+++ server/routes/arenas.ts
@@ -4,11 +4,15 @@ import { getArenas } from "../controllers/arenas";
 import { compareArenas } from "../controllers/compareArenas";
 import { getIdeasOfArena } from "../controllers/arenaIdeas";
 import { getArenaWinRateTrends } from "../controllers/arenaWinRateTrends";
+import { findMatchingArenas } from "../controllers/findMatchingArenas";
+import { compareIdeasWinRate } from "../controllers/compareIdeasWinRate";
 
 const router = Router();
 
 // Arenas
 router.get("/:companyId/arenas", getArenas);
+router.get("/:companyId/find_matching_arenas/:arenaId", findMatchingArenas);
+router.get("/:companyId/compare_win_rate/:arenaId1/:arenaId2", compareIdeasWinRate);
 router.get("/:companyId/compare", compareArenas);
 router.get("/:companyId/:id", getById);
 router.get("/:companyId/:arenaId/ideas", getIdeasOfArena);
