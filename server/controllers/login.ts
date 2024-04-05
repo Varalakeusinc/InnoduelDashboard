8be63b088dd7e152dc2d07e7497f9f5772661207
temelehtone <91484193+temelehtone@users.noreply.github.com>
2024-04-05 17:07:41 +0300

    Fix authentication

--- server/controllers/login.ts
+++ server/controllers/login.ts
@@ -16,6 +16,13 @@ export const loginUser = async (req: Request, res: Response) => {
     const user = await prisma.user_info.findUnique({
         where: {
             id: 3
+        },
+        select: {
+            id: true,
+            email: true,
+            company_id: true,
+            user_role_link: true,
+            name: true
         }
     });
 
@@ -23,17 +30,18 @@ export const loginUser = async (req: Request, res: Response) => {
         return res.status(401).json({ message: "User not found" });
     }
 
+    const isAdmin = user.user_role_link.some((role) => role.role_id === 3);
     const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET as string, {
         expiresIn: '1h',
     });
 
-    res.cookie('token', token, { 
-        httpOnly: true, 
-        sameSite: 'strict', 
+    res.cookie('token', token, {
+        httpOnly: true,
+        sameSite: 'strict',
         maxAge: 60 * 60 * 1000 // 1h
     });
 
-    res.status(201).json({ message: "Login successful" });
+    res.status(201).json({ message: "Login successful", user: { email: user.email, companyId: user.company_id, isAdmin, username: user.name } });
 };
 
 export const logoutUser = async (req: Request, res: Response) => {
