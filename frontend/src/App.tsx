a6395ff193e4717cb86ed920fa34a74acde1f495
Danielevaccari <daniele.j.vaccari@gmail.com>
2024-04-10 21:05:25 +0300

    Conflict fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,32 +7,72 @@ import ArenaPage from "./pages/Arena";
 import Compare from "./pages/Compare";
 import { useAppSelector } from "@/store/hooks";
 import { selectUser } from "@/store/userSlice";
+import { useTranslation } from "react-i18next";
 
 const App = () => {
-    return (
-        <>
+	const { t } = useTranslation();
+	return (
+		<>
 			<Routes>
-				<Route path="/" element={<PrivateRoutes><HomePage /></PrivateRoutes>} />
-				<Route path="/compare" element={<PrivateRoutes><Compare /></PrivateRoutes>} />
-				<Route path="/arenas" element={<PrivateRoutes><Arenas /></PrivateRoutes>} />
-				<Route path="/arena/:id" element={<PrivateRoutes><ArenaPage /></PrivateRoutes>} />
+				<Route
+					path="/"
+					element={
+						<PrivateRoutes>
+							<HomePage />
+						</PrivateRoutes>
+					}
+				/>
+				<Route
+					path="/compare"
+					element={
+						<PrivateRoutes>
+							<Compare />
+						</PrivateRoutes>
+					}
+				/>
+				<Route
+					path="/arenas"
+					element={
+						<PrivateRoutes>
+							<Arenas />
+						</PrivateRoutes>
+					}
+				/>
+				<Route
+					path="/arena/:id"
+					element={
+						<PrivateRoutes>
+							<ArenaPage />
+						</PrivateRoutes>
+					}
+				/>
 				<Route path="/login" element={<LoginPage />} />
 				{/* Add not found component later */}
-				<Route path="*" element={<PrivateRoutes><h1>{t("not_found")}</h1></PrivateRoutes>}
+				<Route
+					path="*"
+					element={
+						<PrivateRoutes>
+							<h1>{t("not_found")}</h1>
+						</PrivateRoutes>
+					}
 				/>
 			</Routes>
-        </>
-    );
+		</>
+	);
 };
 
 interface PrivateRoutesProps {
-    children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 const PrivateRoutes = ({ children }: PrivateRoutesProps) => {
-    const user = useAppSelector(selectUser);
+	const user = useAppSelector(selectUser);
 
-    return user?.companyId === -1 ? <Navigate to="/login" /> : <Layout>{children}</Layout>;
+	return user?.companyId === -1 ? (
+		<Navigate to="/login" />
+	) : (
+		<Layout>{children}</Layout>
+	);
 };
 
 export default App;
