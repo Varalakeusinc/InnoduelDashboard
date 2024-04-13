3e22a2e1ce67dde43046325e426fe1ac03714029
Aynur Rahman Talukdar <128217234+dareDEVil-aynur@users.noreply.github.com>
2024-04-13 21:02:42 +0300

    add middlewares

--- server/routes/reports.ts
+++ server/routes/reports.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { exportExcel } from "../controllers/exportExcel";
+import { middleware } from "../utils/middleware";
 
 const router = Router();
 
 // Excel export
-router.get("/:company_id/excel", exportExcel);
+router.get("/:company_id/excel",
+    middleware.requireAuth,
+    middleware.requireAdmin,
+    exportExcel,
+);
 
 export default router;
