739209fc2bb909e73443c663d7d4c67e84f537fc
Danielevaccari <daniele.j.vaccari@gmail.com>
2024-04-13 14:20:09 +0300

    Added login helper cypress function and fixed existing tests

--- frontend/components/navbar/user-nav.tsx
+++ frontend/components/navbar/user-nav.tsx
@@ -38,53 +38,55 @@ export function UserNav() {
 	};
 
 	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<Button
-					variant="primary"
-					className="relative h-12 w-12 rounded-full"
-				>
-					<Avatar className="h-12 w-12">
-						<AvatarImage src="/avatars/01.png" alt="@shadcn" />
-						<AvatarFallback>
-							{isLoggedIn ? currentUser?.username : ""}
-						</AvatarFallback>
-					</Avatar>
-				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent className="w-56" align="end" forceMount>
-				<DropdownMenuLabel className="font-normal">
-					<div className="flex flex-col space-y-1">
-						<p className="text-sm font-medium leading-none">
-							{isLoggedIn
-								? currentUser?.username
-								: "Please log in"}
-						</p>
-						<p className="text-xs leading-none text-muted-foreground">
-							{isLoggedIn ? currentUser?.email : ""}
-						</p>
-					</div>
-				</DropdownMenuLabel>
-				<DropdownMenuSeparator />
-				<DropdownMenuGroup>
-					<DropdownMenuItem className="cursor-pointer">
-						<CompanySelector />
+		<div data-test-id="userNav-dropdown">
+			<DropdownMenu>
+				<DropdownMenuTrigger asChild>
+					<Button
+						variant="primary"
+						className="relative h-12 w-12 rounded-full"
+					>
+						<Avatar className="h-12 w-12">
+							<AvatarImage src="/avatars/01.png" alt="@shadcn" />
+							<AvatarFallback>
+								{isLoggedIn ? currentUser?.username : ""}
+							</AvatarFallback>
+						</Avatar>
+					</Button>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent className="w-56" align="end" forceMount>
+					<DropdownMenuLabel className="font-normal">
+						<div className="flex flex-col space-y-1">
+							<p className="text-sm font-medium leading-none">
+								{isLoggedIn
+									? currentUser?.username
+									: "Please log in"}
+							</p>
+							<p className="text-xs leading-none text-muted-foreground">
+								{isLoggedIn ? currentUser?.email : ""}
+							</p>
+						</div>
+					</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					<DropdownMenuGroup>
+						<DropdownMenuItem className="cursor-pointer">
+							<CompanySelector />
+						</DropdownMenuItem>
+						<DropdownMenuItem className="cursor-pointer">
+							<LanguageSwitcher />
+						</DropdownMenuItem>
+						<DropdownMenuItem className="cursor-pointer">
+							{t("profile")}
+						</DropdownMenuItem>
+						<DropdownMenuItem className="cursor-pointer">
+							{t("settings")}
+						</DropdownMenuItem>
+					</DropdownMenuGroup>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem onClick={logOut}>
+						{t("log_out")}
 					</DropdownMenuItem>
-					<DropdownMenuItem className="cursor-pointer">
-						<LanguageSwitcher />
-					</DropdownMenuItem>
-					<DropdownMenuItem className="cursor-pointer">
-						{t("profile")}
-					</DropdownMenuItem>
-					<DropdownMenuItem className="cursor-pointer">
-						{t("settings")}
-					</DropdownMenuItem>
-				</DropdownMenuGroup>
-				<DropdownMenuSeparator />
-				<DropdownMenuItem onClick={logOut}>
-					{t("log_out")}
-				</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
+				</DropdownMenuContent>
+			</DropdownMenu>
+		</div>
 	);
 }
