27d9499ee43bea64d479f207bb09d5139e33af85
Anni <anni.keinonen@tuni.fi>
2024-04-17 19:09:30 +0300

    Small visual changes

--- frontend/src/pages/ReportButton.tsx
+++ frontend/src/pages/ReportButton.tsx
@@ -25,9 +25,8 @@ const ReportButton = ({ companyId }: { companyId: number }) => {
       .then((response) => {
         const url = window.URL.createObjectURL(new Blob([response.data])); 
         const link = document.createElement('a');
-
         const timestamp = getFormattedTimestamp();
-        const filename = `innoduel_dashboard_${currentCompanyName}_${timestamp}.xlsx`; // Use current company name
+        const filename = `innoduel_dashboard_${currentCompanyName}_${timestamp}.xlsx`;
 
         link.href = url;
         link.setAttribute('download', filename);
