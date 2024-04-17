84ee2a27ed77bd2f31cb2773d9a11ad22f560edf
Anni <anni.keinonen@tuni.fi>
2024-04-17 18:44:11 +0300

    Add timestamp to frontend

--- frontend/src/pages/ReportButton.tsx
+++ frontend/src/pages/ReportButton.tsx
@@ -4,15 +4,34 @@ import { useTranslation } from 'react-i18next';
 const ReportButton = ({ companyId }: { companyId: number }) => {
   const { t } = useTranslation();
 
+  function getFormattedTimestamp() {
+    const date = new Date();
+    const year = date.getFullYear().toString().substring(2);
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+    return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+  }
+
   const handleExportButtonClick = () => {
     axios.get(`/api/reports/${companyId}/excel`, { 
       responseType: 'blob' 
     })
-    .then((response) => {
+      .then((response) => {
         const url = window.URL.createObjectURL(new Blob([response.data])); 
         const link = document.createElement('a');
+
+        const timestamp = getFormattedTimestamp();
+
+        //const currentCompanyName = useAppSelector(selectCompanyName);
+
+        const filename = `innoduel_dashboard_${timestamp}.xlsx`;
+
+
         link.href = url;
-        link.setAttribute('download', 'report.xlsx');
+        link.setAttribute('download', filename);
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
