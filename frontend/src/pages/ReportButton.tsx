31dc34bc3a7f3f26a794c4f675fb0c340e52512f
Anni <anni.keinonen@tuni.fi>
2024-04-17 18:08:06 +0300

    Change axios import, fix code

--- frontend/src/pages/ReportButton.tsx
+++ frontend/src/pages/ReportButton.tsx
@@ -1,18 +1,14 @@
-import axios from 'axios'; 
+import axios from "@/lib/axios";
 import { useTranslation } from 'react-i18next';
 
 const ReportButton = ({ companyId }: { companyId: number }) => {
   const { t } = useTranslation();
 
   const handleExportButtonClick = () => {
-    console.log("Export button clicked.");
-
     axios.get(`/api/reports/${companyId}/excel`, { 
       responseType: 'blob' 
     })
     .then((response) => {
-        console.log("Excel file retrieved successfully.");
-
         const url = window.URL.createObjectURL(new Blob([response.data])); 
         const link = document.createElement('a');
         link.href = url;
@@ -20,9 +16,9 @@ const ReportButton = ({ companyId }: { companyId: number }) => {
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
+        URL.revokeObjectURL(url);
       })
       .catch((error) => {
-        console.log("Error occurred while downloading Excel file:", error);
         console.error("Error downloading Excel file:", error);
       });
   };
