bf7c645413f1d3dd57183620d34ded5cbcf6023b
Anni <anni.keinonen@tuni.fi>
2024-04-25 18:32:26 +0300

    Create cypress for container and +/- buttons

---
+++ frontend/cypress/e2e/compare.cy.ts
@@ -0,0 +1,28 @@
+import logInHelper from "./basics.cy";
+
+const url = "http://localhost:5173";
+
+const email = "a@a.com";
+const password = "admin";
+
+describe("Compare Component", () => {
+    beforeEach(() => {
+        logInHelper(url, email, password);
+        cy.get("[data-test-id='navigation-compare']").should("exist").click();
+    });
+
+    it("renders the container element", () => {
+        cy.get(".container.mx-auto").should("exist");
+    });
+
+    it("renders the Plus and Minus buttons", () => {
+        cy.get(".fixed.top-40.right-4 > Button").should("have.length", 2);
+    });
+
+    it("adds a new view and removes it when clicking Plus and then Minus buttons", () => {
+        cy.get(".fixed.top-40.right-4 > Button:first-child").click();
+        cy.get(".fixed.top-40.right-4 > Button:nth-child(2)").click();
+    });
+
+    
+});
