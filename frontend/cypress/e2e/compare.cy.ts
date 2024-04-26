ba1119b2a412cb3c1676641e201c739852517fae
Anni <anni.keinonen@tuni.fi>
2024-04-26 08:16:15 +0300

    Clean up and add note

--- frontend/cypress/e2e/compare.cy.ts
+++ frontend/cypress/e2e/compare.cy.ts
@@ -12,25 +12,16 @@ describe("Compare Component", () => {
     });
 
     it("selects a specific company and loads arenas", () => {
-        //cy.get("[data-test-id='navigation-home']").should("exist").click();
-        cy.visit(url);
-
         cy.get("[data-test-id='userNav-dropdown']").click();
         cy.wait(1000); // Wait for the dropdown to open
         cy.get("[data-test-id='company-selector-button']").click();
         cy.contains("Ilari admin").click();
         cy.wait(2000);
 
-        cy.get("[data-test-id='navigation-compare']").should("exist").click();
-
-
         cy.get("[aria-label='Select Arena']").should('exist').eq(0).click({ force: true });
+        // IT SHOULD FIND THIS ARENA
         cy.contains("Vahvuudet").click(); 
-
-
     });
-    
-    
 
     it("renders the container element", () => {
         cy.get(".container.mx-auto").should("exist");
